979b220fbecc79d7d67e98e5492d948bb3b05c12
Fáusio Paco <fausio18.paco@gmail.com>
2025-11-12 09:44:43 +0200

    added main packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image']
-})
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@nuxt/fonts',
+    '@nuxt/scripts', 
+    'nuxt-svgo',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/robots',
+    'nuxt-seo-utils',
+    'nuxt-link-checker',
+    'nuxt-qrcode',
+  ],
+});
