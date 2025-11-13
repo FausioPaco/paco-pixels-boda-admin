04fc6032ff0abb4e2d0d759d9d7841ebe6786f01
Fáusio Paco <fausio18.paco@gmail.com>
2025-11-13 16:33:06 +0200

    added guest and desks main pages

---
+++ app/pages/admin/convidados/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+useHead({
+  title: 'Convidados',
+  meta: [
+    {
+      name: 'description',
+      content: 'Convidados',
+    },
+  ],
+});
+
+definePageMeta({
+  name: 'Convidados',
+  middleware: ['auth'],
+});
+</script>
+
+<template>
+  <div>
+    <!-- Breadcumb -->
+    <BaseBreadcumb>
+      <NuxtLink to="/admin">Visão Geral</NuxtLink>
+      <IconChevronRight
+        :font-controlled="false"
+        class="text-grey-400 size-[16px]"
+      />
+      <span class="active">Convidados</span>
+    </BaseBreadcumb>
+
+    <!-- Counters -->
+    <!-- <LazyGuestsManagement /> -->
+  </div>
+</template>
---
+++ app/pages/admin/mesas/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+useHead({
+  title: 'Mesas',
+  meta: [
+    {
+      name: 'description',
+      content: 'Mesas',
+    },
+  ],
+});
+
+definePageMeta({
+  name: 'Mesas',
+  middleware: ['auth'],
+});
+</script>
+
+<template>
+  <div>
+    <!-- Breadcumb -->
+    <BaseBreadcumb>
+      <NuxtLink to="/admin">Visão Geral</NuxtLink>
+      <IconChevronRight
+        :font-controlled="false"
+        class="text-grey-400 size-[16px]"
+      />
+      <span class="active">Mesas</span>
+    </BaseBreadcumb>
+
+    <!-- Counters -->
+    <!-- <LazyDesksManagement /> -->
+  </div>
+</template>
