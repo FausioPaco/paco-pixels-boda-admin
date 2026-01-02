2280ae7bdc5fd717fecd141f251477736aba1793
Fáusio Paco <fausio18.paco@gmail.com>
2026-01-02 14:40:14 +0200

    added page for suppliers

---
+++ app/pages/admin/fornecedores/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+useHead({
+  title: 'Gestão de Fornecedores',
+  meta: [
+    {
+      name: 'description',
+      content: 'Gestão de Fornecedores',
+    },
+  ],
+});
+
+definePageMeta({
+  name: 'Gestão de Fornecedores',
+  pageName: 'Gestão de Fornecedores',
+  middleware: ['auth'],
+});
+</script>
+
+<template>
+  <div>
+    <!-- Breadcumb -->
+    <BaseBreadcumb>
+      <NuxtLink to="/admin">Visão Geral</NuxtLink>
+      <IconChevronRight
+        :font-controlled="false"
+        class="text-grey-400 size-[16px]"
+      />
+      <span class="active">Fornecedores</span>
+    </BaseBreadcumb>
+
+    <!-- Suppliers  Management -->
+    <LazySuppliersManagement />
+  </div>
+</template>
