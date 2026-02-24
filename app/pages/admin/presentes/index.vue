5dd8c637d6e71c8b6110e4b749fadaa8556cc11d
Fáusio Paco <fausio18.paco@gmail.com>
2026-02-24 19:32:06 +0200

    added gifts list link

---
+++ app/pages/admin/presentes/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+useHead({
+  title: 'Lista de Presentes',
+  meta: [
+    {
+      name: 'description',
+      content: 'Lista de Presentes',
+    },
+  ],
+});
+
+definePageMeta({
+  name: 'Lista de Presentes',
+  pageName: 'Lista de Presentes',
+  middleware: ['auth'],
+});
+</script>
+
+<template>
+  <div>
+    <!-- Breadcumb -->
+    <BaseBreadcumb>
+      <NuxtLink to="/admin">Visão Geral</NuxtLink>
+      <IconChevronRight
+        :font-controlled="false"
+        class="text-grey-400 size-[16px]"
+      />
+      <span class="active">Lista de Presentes</span>
+    </BaseBreadcumb>
+
+    <!-- Event Gift List Management -->
+    <LazyEventGiftListManagement />
+  </div>
+</template>
